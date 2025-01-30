50f830188478d6fc83e5053485a889307c7d8919
mansadev <144854769+mansadev@users.noreply.github.com>
2025-01-30 22:21:09 +0100

    Delete packaging directory

--- packaging/rpm/scripts/preclients.sh
+++
@@ -1,3 +0,0 @@
-getent group foundationdb >/dev/null || groupadd -r foundationdb >/dev/null
-getent passwd foundationdb >/dev/null || useradd -c "FoundationDB" -g foundationdb -s /bin/false -r -d /var/lib/foundationdb foundationdb >/dev/null
-exit 0
